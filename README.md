44b3580b9f3e2551c16c4a81a18f4871b0b80e15
GBRodrickTed <115361910+GBRodrickTed@users.noreply.github.com>
2026-03-09 22:04:54 -0400

    frawd

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Ricochet Assist
 A mod that changes the ricochet aim assist, increase sharpshooters ricochet count, and more.
-
-## Changelog
-* 1.0.0
-  * Initial release.
--- RicochetAssist/RicochetMorty.cs
+++ RicochetAssist/RicochetMorty.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.UIElements;
+using ULTRAKILL.Enemy;
+using ULTRAKILL.Portal;
 
 namespace RicochetAssist
 {
@@ -36,28 +38,144 @@ namespace RicochetAssist
             float minDist = float.PositiveInfinity;
             GameObject mainObject = null;
             Transform target = null;
+            Vector3 aimPos = Vector3.zero;
             EnemyIdentifier eid = null;
             RevolverBeam revb = __instance;
-            if (CoinList.Instance.revolverCoinsList.Count > 0 && shouldTargetCoin)
+
+            // Based on the coin vision implementation
+            Vision vision = new Vision(revb.transform.position, new VisionTypeFilter(new TargetType[]
             {
-                foreach (Coin coin in CoinList.Instance.revolverCoinsList)
+                TargetType.PLAYER,
+                TargetType.COIN,
+                TargetType.ENEMY,
+                TargetType.EXPLOSIVE,
+                TargetType.GLASS
+            }));
+            MonoSingleton<PortalManagerV2>.Instance.TargetTracker.RegisterVision(vision, revb.destroyCancellationToken);
+            // This is needed to give an index to the vision
+            // Don't know how the coin imp get's away with not doing this.
+            MonoSingleton<PortalManagerV2>.Instance.TargetTracker.UpdateData();
+            // Based on the condition checks, It seems that the target data predicate information checks for conditions relating to the query beforehand for convinence / optimization (citations needed), but these could be checked later on if one so pleased. I think.
+
+            VisionQuery playerQuery = new VisionQuery("RicPlayer", (TargetDataRef t) => t.target.Type == TargetType.PLAYER && !t.IsObstructed(revb.transform.position, LayerMaskDefaults.Get(LMD.Environment), false));
+            // Don't know why they have a check just for gayrod specifically but I won't judge
+            VisionQuery enemyQuery = new VisionQuery("RicEnemy", (TargetDataRef t) => t.target.Type == TargetType.ENEMY && (t.target.EID.enemyType == EnemyType.Geryon || !t.IsObstructed(revb.transform.position, LayerMaskDefaults.Get(LMD.Environment), false)));
+            VisionQuery coinQuery = new VisionQuery("RicCoin", (TargetDataRef t) => t.target.Type == TargetType.COIN && !t.IsObstructed(revb.transform.position, LayerMaskDefaults.Get(LMD.Environment), false));
+            VisionQuery explosiveQuery = new VisionQuery("RicExplosive", (TargetDataRef t) => t.target.Type == TargetType.EXPLOSIVE && !t.IsObstructed(revb.transform.position, LayerMaskDefaults.Get(LMD.Environment), false));
+            /*VisionQuery glassQuery = new VisionQuery("RicGlass", delegate (TargetDataRef t)
+            {
+                PhysicsCastResult hit;
+                PortalTraversalV2[] array2;
+                return t.target.Type == TargetType.GLASS && (!t.IsObstructed(base.transform.position, this.env_lm, false, out hit, out array2) || this.GlassHitCheck(t, hit));
+            });*/
+
+            TargetDataRef targetDataRef;
+
+            vision.UpdateSourcePos(revb.transform.position);
+
+            
+            //Debug.LogError(vision.visionIndex + " GRAAAAAAAAAAAAAAAAAAH");
+
+            // Seems to prefer the closest target, Which is fine for what im doing, but a bit limiting.
+            if (vision.TrySee(coinQuery, out targetDataRef) && shouldTargetCoin)
+            {
+                //revb.gameObject.transform.forward = Vector3.up;
+                Coin coin = targetDataRef.target.GameObject.GetComponent<Coin>();
+                if (coin != null && !coin.shot && !coin.shotByEnemy)
+                {
+                    target = coin.transform;
+                    aimPos = targetDataRef.portalMatrix.MultiplyPoint3x4(target.position);
+                    if (!Utils.WithinFOV(revb.transform.forward, (aimPos - revb.transform.position).normalized, ricFOV))
+                    {
+                        target = null;
+                    }
+                }
+            }
+
+            if (vision.TrySee(explosiveQuery, out targetDataRef))
+            {
+                target = targetDataRef.target.GameObject.transform;
+                aimPos = targetDataRef.portalMatrix.MultiplyPoint3x4(target.position);
+                if (!Utils.WithinFOV(revb.transform.forward, (aimPos - revb.transform.position).normalized, ricFOV))
+                {
+                    target = null;
+                }
+            }
+
+            if (vision.TrySee(enemyQuery, out targetDataRef) && target == null)
+            {
+                eid = targetDataRef.target.EID;
+
+                if (eid != null && !eid.dead && (eid.gameObject) && !(eid.blessed))
+                {
+                    if (eid.weakPoint != null && eid.weakPoint.activeInHierarchy)
+                    {
+                        target = eid.weakPoint.transform;
+                        aimPos = targetDataRef.portalMatrix.MultiplyPoint3x4(target.position);
+                        if (!Utils.WithinFOV(revb.transform.forward, (aimPos - revb.transform.position).normalized, ricFOV))
+                        {
+                            target = null;
+                        }
+                    }
+                    if (target == null)
+                    {
+                        EnemyIdentifierIdentifier eidid = eid.GetComponentInChildren<EnemyIdentifierIdentifier>();
+                        if (eidid && eidid.eid && eidid.eid == eid)
+                        {
+                            target = eidid.transform;
+                            aimPos = targetDataRef.portalMatrix.MultiplyPoint3x4(target.position);
+                            if (!Utils.WithinFOV(revb.transform.forward, (aimPos - revb.transform.position).normalized, ricFOV))
+                            {
+                                target = null;
+                            }
+                        }
+                        if (target == null)
+                        {
+                            target = eid.transform;
+                            aimPos = targetDataRef.portalMatrix.MultiplyPoint3x4(target.position);
+                            if (!Utils.WithinFOV(revb.transform.forward, (aimPos - revb.transform.position).normalized, ricFOV))
+                            {
+                                target = null;
+                            }
+                        }
+                    }
+                }
+                // Im assuming this converts from whereitliterallyis-space to whereitisrelitivetotheportalsaroundit-space
+                // It kinda makes sense when you think about it
+                //aimPos = targetDataRef.portalMatrix.MultiplyPoint3x4(target.position);
+
+                //revb.transform.forward = (aimPos - revb.transform.position).normalized;
+                //Debug.LogError("Epic Vector Battles of History! " + target.position + " vershish " + aimPos + ". Begin!");
+            }
+
+            if (target)
+            {
+                revb.transform.forward = (aimPos - revb.transform.position).normalized;
+            }
+
+            return shouldVanillaAimAssist;
+            /*if (CoinTracker.Instance.revolverCoinsList.Count > 0 && shouldTargetCoin)
+            {
+                foreach (Coin coin in CoinTracker.Instance.revolverCoinsList)
                 {
                     if (coin != null && (!coin.shot || coin.shotByEnemy))
                     {
-                        Vector3 thing2fromthing1 = (coin.transform.position - revb.transform.position);
-                        if (!Utils.WithinFOV(revb.transform.forward, (thing2fromthing1), ricFOV))
+                        PhysicsCastResult phzHit;
+                        if (PortalPhysicsV2.Raycast(revb.transform.position, coin.transform.position, out phzHit, float.PositiveInfinity, LayerMaskDefaults.Get(LMD.Environment)))
                         {
-                            continue;
-                        }
-                        float dist = thing2fromthing1.sqrMagnitude;
-                        if (dist < minDist)
-                        {
-                            RaycastHit rayHit;
-                            if (!Physics.Raycast(revb.transform.position, thing2fromthing1, out rayHit, Vector3.Distance(revb.transform.position, coin.transform.position) - 0.5f, LayerMaskDefaults.Get(LMD.Environment)))
+                            if (!Utils.WithinFOV(revb.transform.forward, phzHit.direction, ricFOV))
+                            {
+                                continue;
+                            }
+                            
+                            if (phzHit.distance < minDist)
                             {
                                 mainObject = coin.gameObject;
-                                minDist = dist;
+                                minDist = phzHit.distance;
                             }
+                        } else
+                        {
+                            continue;//
                         }
                     }
                 }
@@ -67,6 +185,42 @@ namespace RicochetAssist
                 minDist = float.PositiveInfinity;
                 foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
                 {
+                    if (eid != null && !eid.dead && (eid.gameObject) && !(eid.blessed))
+                    {
+                        if (eid.weakPoint != null && eid.weakPoint.activeInHierarchy)
+                        {
+                            target = eid.weakPoint.transform;
+                        }
+                        else
+                        {
+                            EnemyIdentifierIdentifier eidid = eid.GetComponentInChildren<EnemyIdentifierIdentifier>();
+                            if (eidid && eidid.eid && eidid.eid == eid)
+                            {
+                                target = eidid.transform;
+                            }
+                            else
+                            {
+                                target = eid.transform;
+                            }
+                        }
+                        PhysicsCastResult phzHit;
+                        if (PortalPhysicsV2.Raycast(revb.transform.position, target.position - revb.transform.position, out phzHit, float.PositiveInfinity, LayerMaskDefaults.Get(LMD.Environment)))
+                        {
+                            if (phzHit.distance < minDist)
+                            {
+                                mainObject = target.gameObject;
+                                minDist = phzHit.distance;
+                            }
+                        }
+                        else
+                        {
+                            eid = null;
+                        }
+                    }
+
+
+
+*//*
                     Vector3 thing2fromthing1 = enemy.transform.position - revb.transform.position;
                     float dist = (thing2fromthing1).sqrMagnitude;
                     if (dist < minDist)
@@ -109,14 +263,14 @@ namespace RicochetAssist
                         {
                             eid = null;
                         }
-                    }
+                    }*//*
                 }
             }
             if (mainObject != null)
             {
                 revb.gameObject.transform.LookAt(mainObject.transform.position);
             }
-            return shouldVanillaAimAssist;
+            return shouldVanillaAimAssist;*/
         }
         [HarmonyPatch(typeof(RevolverBeam), nameof(RevolverBeam.Start))]
         [HarmonyPrefix]
@@ -129,7 +283,7 @@ namespace RicochetAssist
                     __instance.ricochetAmount += railBounceAmount;
                     return true;
                 }
-                if (__instance.canHitProjectiles)
+                if (__instance.canHitProjectiles)//
                 {
                     __instance.ricochetAmount += ricBounceAmount;
                     return true;
